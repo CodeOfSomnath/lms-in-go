ea6303bfdf93bc3fd87aacf7fac057009b781116
Somnath Nandi <123479436+CodeOfSomnath@users.noreply.github.com>
2023-12-01 21:00:20 +0530

    implementing library function

--- lms/library.go
+++ lms/library.go
@@ -1,8 +1,59 @@
 package lms
 
+
 type Library struct {
-	storedBooks []Book
+	storedBooks   []Book
 	borrowedBooks []Book
 }
 
 // for creating new library
+func NewLibrary() Library {
+	return Library{
+		storedBooks:   make([]Book, 0),
+		borrowedBooks: make([]Book, 0),
+	}
+}
+
+// adding books to library
+func (l *Library) AddBook(name, author string) {
+	book := NewBook(name, author)
+	l.storedBooks = append(l.storedBooks, book)
+}
+
+func (l *Library) RemoveBook(name, author string) *Book {
+	book := NewBook(name, author)
+	for _, b := range l.storedBooks {
+		if book == b {
+			return &book
+		}
+	}
+	return nil
+}
+
+func (l *Library) BorrowBook(name, author string) *Book {
+	book := NewBook(name, author)
+	for i, b := range l.storedBooks {
+		if book == b {
+			books := l.storedBooks[:i]
+			books = append(books, l.storedBooks[i+1:len(l.storedBooks)]...)
+			l.storedBooks = books
+			l.storedBooks = append(l.storedBooks, book)
+			return &book
+		}
+	}
+	return nil
+}
+
+func (l *Library) SubmitBook(name, author string) bool {
+	book := NewBook(name, author)
+	for i, b := range l.borrowedBooks {
+		if book == b {
+			books := l.borrowedBooks[:i]
+			books = append(books, l.borrowedBooks[i+1:len(l.borrowedBooks)]...)
+			l.borrowedBooks = books
+			l.storedBooks = append(l.storedBooks, book)
+			return true
+		}
+	}
+	return false
+}
--- lms/library_system.go
+++ lms/library_system.go
@@ -1,1 +1,38 @@
 package lms
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func basicOptions() {
+	println("Options are:")
+	println("\t1.Show options")
+	println("\t2.Add Books")
+	println("\t3.Remove Books")
+	println("\t4.Borrow Books")
+	println("\t5.Submit Books")
+	println("\t6.Exit")
+}
+
+func TakeNameAndAuthor() (name, author string) {
+	fmt.Print("Enter book's title:")
+	fmt.Scanln(&name)
+	fmt.Print("Enter book's author:")
+	fmt.Scanln(&author)
+	return
+}
+
+func TakeOption() int {
+	var inp string
+	fmt.Println("Enter a option:")
+	fmt.Scanln(&inp)
+	inp = strings.TrimSpace(inp)
+	value, err := strconv.Atoi(inp)
+	if err != nil {
+		return 1
+	}
+	return value
+}
