c440992799321d1db06f51b0a54fc98fdf281b21
Somnath Nandi <123479436+CodeOfSomnath@users.noreply.github.com>
2023-12-01 21:50:22 +0530

    adding main function

--- main.go
+++ main.go
@@ -1,15 +1,9 @@
 package main
 
-import "github.com/CodeOfSomnath/lms-in-go/lms"
+import (
+	"github.com/CodeOfSomnath/lms-in-go/lms"
+)
 
 func main() {
-	b1 := lms.NewBook("Hello", "Somnath")
-	// b2 := lms.NewBook("Hello", "Somnath")
-	println(b1)
-	l1 := []int{1,2,3,4,5,6}
-	l2 := l1[0:1]
-	l2 = append(l2, l1[2:]...)
-	for _, v := range l2 {
-		print(v)
-	}
+	lms.StartLms()
 }
